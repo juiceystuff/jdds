69da76481d88965d0fa0bff7df5709b6cca1c564
Gareth <jengarweb@gmail.com>
2015-07-27 23:30:40 +0100

    update

--- index.php
+++ index.php
@@ -1,14 +1,36 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
 	<link rel="stylesheet" type="text/css" href="css/uikit.almost-flat.css">
 	<link rel="stylesheet" type="text/css" href="css/custom.css">
+<head>
+	<title>...</title>
 </head>
-<body>
-	<section class="sect1">
-		...
-	</section>
+
+<div class="uk-container">
+	<div class="uk-grid uk-grid-margin">
+		<div class="uk-width-medium-1-2">
+			<img src="images/logo.svg" class="js-logo" alt="JDDS Logo">
+		</div>
+		<div class="uk-width-medium-1-2">
+			<nav>
+				<ul class="uk-nav">
+					<li class="uk-active"><a href="#">Home</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</div>
+
+<body class="babyBG">
+	<div class="uk-container uk-container-center">
+		<div class="uk-width-medium-1-1">
+			<h1>BEAUTIFUL</h1>
+		</div>
+	</div>
 
 </body>
 </html>
